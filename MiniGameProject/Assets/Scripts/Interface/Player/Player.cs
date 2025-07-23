1a706db71202badce8e7a234520819c42db06d45
zjaxjrhkd <zjaxjrhkd100@gmail.com>
2025-07-23 18:56:04 +0900

    [feat] 장애물 피하기 맵 생성 로직 구현 #5
    필요한 기능
    장애물 피하기 미니 게임에서 맵 생성 로직을 구현한다.
    구현 방법
    맵 오브젝트를 3분할 하여 이동후 인덱스를 변경하는 것으로 무한히 재생한다.
    TODO
    
    1. 장애물 피하기 맵 생성 클래스 추가

--- MiniGameProject/Assets/Scripts/Interface/Player/Player.cs
+++ MiniGameProject/Assets/Scripts/Interface/Player/Player.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -5,6 +6,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IMove, IClothes
 {
+    public enum PlayerState
+    {
+        Idle,
+        Walking,
+        Running,
+        Jumping
+    }
+    public enum PlayerGameMode
+    {
+        None,
+        StackBlock,
+        Run
+    }
+    public PlayerState playerState = PlayerState.Idle;
+    public PlayerGameMode playerGameMode = PlayerGameMode.None;
+
     private float _moveSpeed = 5f;
 
     private Vector2 _movementDirection;
@@ -19,6 +36,33 @@ public class Player : MonoBehaviour, IMove, IClothes
         private set { _moveSpeed = value; }
     }
     public void OnMove()
+    {
+        switch (playerGameMode)
+        {
+            case PlayerGameMode.StackBlock:
+                // Implement stack block movement logic here
+                Debug.Log("Player is in StackBlock mode");
+                break;
+            case PlayerGameMode.Run:
+                // Implement run mode movement logic here
+                Debug.Log("Player is in Run mode");
+                break;
+            default:
+                // Default movement logic
+                MapMove();
+                break;
+        }
+    }
+
+    public void MapMove()
+    { 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+        transform.position += new Vector3(movementDirection.x, movementDirection.y, 0) * MoveSpeed * Time.deltaTime;
+    }
+
+    public void RunMove()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
